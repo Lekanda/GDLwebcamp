5243247f0b9b73ced1e34c6de4e8f4b048caf77a
lekanda <abernaolao@gmail.com>
2021-06-21 17:19:32 +0200

    547.Variables del DOM para JS.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@
   "use strict";
 
   document.addEventListener('DOMContentLoaded', function () {
+    // console.log("listo");
 
-    console.log("listo");
+    // Campos Usuario
+    var nombre = document.getElementById('#nombre');
+    var apellido = document.getElementById('#apellido');
+    var email = document.getElementById('#email');
+
+    // Campos Pases
+    var pase_dia = document.getElementById('#pase_dia');
+    var pase_completo = document.getElementById('#pase_completo');
+    var pase_dosdias = document.getElementById('#pase_dosdias');
+
+    // Botones y Divs
+    var calcular = document.getElementById('#calcular');
+    var errorDiv = document.getElementById('#error');
+    var btnRegistro = document.getElementById('#btnRegistro');
+    var resultado = document.getElementById('#lista-productos');
+
 
 
 
