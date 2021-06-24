a1206f2ac210e87a753cc982cd16382cd4daa5d7
lekanda <abernaolao@gmail.com>
2021-06-24 13:31:35 +0200

    558.Ajustar coordenadas en leaflet.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@
 
   document.addEventListener('DOMContentLoaded', function () {
 
-    var map = L.map('mapa').setView([51.505, -0.09], 13);
+    var map = L.map('mapa').setView([43.127173, -2.766173], 17);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([43.127173, -2.766173]).addTo(map)
+      .bindPopup('Lekanda.Net')
       .openPopup();
 
 
