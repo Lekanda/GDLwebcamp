b99aabe0e429f7427a851961ebfa3cbc06cf965d
lekanda <abernaolao@gmail.com>
2021-07-07 17:44:42 +0200

    623.Consulta a DB.

--- calendario.php
+++ calendario.php
@@ -1,10 +1,34 @@
 <?php include_once '../GDLwebcampPHP/includes/templates/header.php' ?>
 
+  <section class="seccion contenedor">
+    <h2>Calendario de Eventos</h2>
 
+    <?php
+      try {
+        require_once('includes/funciones/db_conexion.php');
+        $sql = "SELECT * FROM eventos";
+        $resultado = $conn->query($sql);
+        echo $resultado;
+      } catch (\Throwable $th) {
+        echo $th->getMessage();
+      }
 
-<section class="seccion contenedor">
-  <h2>Calendario de Eventos</h2>
+    ?>
 
-</section>
+    <div class="calendario">
+      <?php
+        $eventos=$resultado->fetch_assoc(); // Trae el primer registro. Para uun arreglo asociativo
+      ?>
+
+      <pre>
+        <?php var_dump($eventos) ?>
+      </pre>
+    </div>
+
+    <?php
+      $conn->close();
+    ?>
+
+  </section>
 
 <?php include_once '../GDLwebcampPHP/includes/templates/footer.php' ?>
