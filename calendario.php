79a6e11ae4aa67099492412ce6b6e3b41c6d971d
lekanda <abernaolao@gmail.com>
2021-07-09 17:23:44 +0200

    627.Imprimir fechas de eventos de la DB.

--- calendario.php
+++ calendario.php
@@ -39,16 +39,32 @@
                   'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
                 );
 
-                $calendario[$categoria][]= $evento;
+                $calendario[$fecha][]= $evento;
 
                 ?>
             <?php } ?>
+
+            <?php
+              //  Imprime todos los eventos
+              foreach ($calendario as $dia => $lista_eventos) { ?>
+                <h3>
+                  <i class="fa fa-calendar"></i>
+                  <?php
+                    setlocale(LC_TIME, 'spanish');
+                    echo utf8_encode(strftime("%A, %d de %B del %Y",strtotime($dia)));
+                  ?>
+                </h3>
+
+
+
+
+
+              <?php } ?>
+            ?>
+
             <pre>
               <?php var_dump($calendario);?>
             </pre>
-
-
-
       </div>
 
       <?php
--- includes/funciones/db_conexion.php
+++ includes/funciones/db_conexion.php
@@ -4,4 +4,5 @@
   if ($conn->connect_error) {
     echo $error->conn->connect_error;
   }
+  $conn->set_charset("utf8");// Para caracteres del castellano.
 ?>
