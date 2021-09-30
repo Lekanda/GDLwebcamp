66fd9fe337ad07a9e49308fb23f4412a9603fbe3
lekanda <abernaolao@gmail.com>
2021-09-30 17:58:25 +0200

    644.Funcion para convertir a JSON productos y boletos del usuario.

---
+++ includes/funciones/funciones.php
@@ -0,0 +1,26 @@
+<?php
+
+function productos_json(&$boletos, &$camisas = 0, &$etiquetas = 0){
+    $dias = array(0 => 'un_dia', 1 => 'pase_completo', 2 => 'pase_2dias');
+    // Combina 2 arrays
+    $total_boletos = array_combine($dias, $boletos);
+
+    $json = array();
+
+    foreach($total_boletos as $key => $boletos):
+        if ((int) $boletos > 0) {
+          $json[$key] = (int) $boletos;
+        }
+    endforeach;
+
+    return json_encode($json);
+
+
+
+
+    echo "<pre>";
+      var_dump($total_boletos);
+    echo "</pre>";
+
+
+}
--- validar_registro.php
+++ validar_registro.php
@@ -11,10 +11,20 @@
       $total = $_POST['total_pedido'];
       $fecha = date('Y-m-d H:i:s');
 
+      //Pedidos
+      $boletos = $_POST['boletos'];
+      $camisas = $_POST['pedido_camisas'];
+      $etiquetas = $_POST['pedido_etiquetas'];
+      include_once '../GDLwebcampPHP/includes/funciones/funciones.php';
+      $pedido = productos_json($boletos,$camisas,$etiquetas);
+      // echo "<pre>";
+      //   var_dump($pedido);
+      // echo "</pre>";
+
   ?>
-    <pre>
-      <?php var_dump($_POST); ?>
-    </pre>
+    <!-- <pre>
+      <?php var_dump($boletos); ?>
+    </pre> -->
   <?php endif; ?>
 
 </section>
